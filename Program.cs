07791ba746211da589f001aa2044ee6b7673b38b
Thiago Lanza <lanzathm@gmail.com>
2021-12-11 22:08:50 -0300

    Program finished

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+Start();
+
+void Start()
+{
+    Console.Clear();
+    Console.WriteLine("Intercepting millitary satellites\n");
+    LoadingBar();
+
+    Console.WriteLine("\nDecrypting intercepted information\n");
+    LoadingBar();
+
+    Console.WriteLine("\nBreaking super secure passwords\n");
+    LoadingBar();
+
+    Menu();
+}
+
+void Menu()
+{
+    Console.WriteLine("\nDo you really want to Hack NASA and steal data ? (Y/n)");
+    char answer = char.Parse(Console.ReadLine().ToLower());
+
+    if (answer == 'n') System.Environment.Exit(0);
+
+    Console.WriteLine("\nHacking Nasa, please wait\n");
+    LoadingBar();
+
+    Console.WriteLine("NASA SUCCESSFULLY HACKED, YOU HAVE 1 HOUR TO RUNAWAY FROM YOUR COUNTRY");
+    Console.ReadLine();
+}
+
+void LoadingBar()
+{
+    Thread.Sleep(200);
+    string LoadingBar = "#";
+    for (int i = 0; i < 50; i++)
+    {
+        Console.SetCursorPosition(0, Console.CursorTop - 1);
+        LoadingBar += "#";
+        Console.WriteLine(LoadingBar);
+        Thread.Sleep(150);
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Hacking Nasa (It's just a joke, Bro lol)
+
+**This is just a joke to "hack nasa" program, like the ones you see in afternoon movies session xD**
+
+#### How to use
+
+Just run `dotnet run` or the `.exe` file in bin/ directory, have fun (:
